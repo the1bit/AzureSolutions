833e62d42b044b1c7ecc4650173ec035951e8364
Tibor Kiss <tibor.kiss@t-systems.com>
2018-06-14 15:26:08 +0200

    Documentation

--- azdns/azdns.sh
+++ azdns/azdns.sh
@@ -2,15 +2,15 @@
 
 # Check log directory
 if [ ! -d "/var/log/azdns" ]; then
-	# Create Log directory
-	mkdir /var/log/azdns
+        # Create Log directory
+        mkdir /var/log/azdns
 fi
 
 # set date
 DATE=`date +%Y%m%d%H%M%S`
 
 # Start logging
-exec 1>"/var/log/azdns/azdns$DATE.log" 2>&1 
+exec 1>"/var/log/azdns/azdns$DATE.log" 2>&1
 echo "$(date) : ##################################################" >&1
 echo "$(date) : ##################################################" >&1
 echo "$(date) : ##################################################" >&1
@@ -40,37 +40,37 @@ az login --service-principal -u $clientID -p $clientSecret -t $tenant
 if [ $? = 0 ];
 then
 
-	echo "$(date) : # Login success" >&1
-	# Login success
-	echo "$(date) : # Set default subscription" >&1
-	# Set default subscription
-	az account set  --subscription $subscriptionID
-	if [ $? = 0 ];
-	then
-		echo "$(date) : # Default subscription has been set" >&1
-		# Default subscription has been set
-		echo "$(date) : # Get current Puplic IP from Internet" >&1
-		# Get current Puplic IP from Internet
-		pip=$(curl ipinfo.io/ip)
-		if [ ${#pip} -gt 9 ]; then
-			currentPIP=$(az network dns record-set a show -g "$dnsResourceGroup" -z "$zoneName" -n "$aRecordName" --query "arecords[0].ipv4Address" | cut -d '"' -f 2)
-			if [[ $pip != $currentPIP ]];
-			then
-					echo "Current IP in Azure $currentPIP is different than my PIP: $pip. I update Azure now."
-					az network dns record-set a update -g "$dnsResourceGroup" -z "$zoneName" -n "$aRecordName" --set arecords[0].ipv4Address=$pip	
-			else
-					echo "Current IP in Azure $currentPIP is same than my PIP: $pip. Not necessary to update"
-			fi
-		else
-			echo "Puplic IP is not valid"
-		fi
-	else
-		echo "something went wrong during Subscription setting"
-	fi
-		echo "$(date) : # Logout from Azure" >&1
-		# Logout from Azure
-	az logout
+        echo "$(date) : # Login success" >&1
+        # Login success
+        echo "$(date) : # Set default subscription" >&1
+        # Set default subscription
+        az account set  --subscription $subscriptionID
+        if [ $? = 0 ];
+        then
+                echo "$(date) : # Default subscription has been set" >&1
+                # Default subscription has been set
+                echo "$(date) : # Get current Puplic IP from Internet" >&1
+                # Get current Puplic IP from Internet
+                pip=$(curl ipinfo.io/ip)
+                if [ ${#pip} -gt 9 ]; then
+                        currentPIP=$(az network dns record-set a show -g "$dnsResourceGroup" -z "$zoneName" -n "$aRecordName" --query "arecords[0].ipv4Address" | cut -d '"' -f 2)
+                        if [[ $pip != $currentPIP ]];
+                        then
+                                        echo "Current IP in Azure $currentPIP is different than my PIP: $pip. I update Azure now."
+                                        az network dns record-set a update -g "$dnsResourceGroup" -z "$zoneName" -n "$aRecordName" --set arecords[0].ipv4Address=$pip
+                        else
+                                        echo "Current IP in Azure $currentPIP is same than my PIP: $pip. Not necessary to update"
+                        fi
+                else
+                        echo "Puplic IP is not valid"
+                fi
+        else
+                echo "something went wrong during Subscription setting"
+        fi
+                echo "$(date) : # Logout from Azure" >&1
+                # Logout from Azure
+        az logout
 else
-	echo "something went wrong during Azure login"
+        echo "something went wrong during Azure login"
 fi
 echo "END"
