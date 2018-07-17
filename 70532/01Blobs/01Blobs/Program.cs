829ec1924758af5ab7df7604b67a533d58b14030
Tibor Kiss <tibor.kiss@t-systems.com>
2018-07-17 14:24:54 +0200

    70532

--- 70532/01Blobs/01Blobs/Program.cs
+++ 70532/01Blobs/01Blobs/Program.cs
@@ -21,7 +21,7 @@ namespace _01Blobs
             // Parse connection string
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageconnection);
 
-            // Client
+            // Client for blob
             CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
 
             // Get container list
@@ -30,17 +30,104 @@ namespace _01Blobs
             // Create container if does not exist
             container.CreateIfNotExists();
 
+
+            // List container attributes
+            //ListAttributes(container);
+
+            // Set new attributes - metadata
+            //SetMetadata(container);
+
+            //List Metadata of container
+            //ListMetadata(container);
+
+            //Copy file
+            //CopyBlob(container, "lights.jpg");
+
+            Console.ReadKey();
+        }
+
+        static void UploadBlob(CloudBlobContainer container, string fileName, string fileWithPath)
+        {
             // Get blob
-            CloudBlockBlob blockBlob = container.GetBlockBlobReference("70-532-questions.pdf");
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
 
             // Open file and upload it
-            using (var fileStream = System.IO.File.OpenRead(@"D:\Development\70-532-questions.pdf"))
+            using (var fileStream = System.IO.File.OpenRead(fileWithPath))
             {
                 // Upload filestream
                 blockBlob.UploadFromStream(fileStream);
             }
+        }
 
-            Console.ReadKey();
+        static void ListAttributes(CloudBlobContainer container)
+        {
+            // Get container existing attrubutes
+            container.FetchAttributes();
+
+            // Write attributes out to screen
+            Console.WriteLine("Container Name: " + container.Name);
+            Console.WriteLine("Container Primary URL: " + container.StorageUri.PrimaryUri.ToString());
+            Console.WriteLine("Last modified: " + container.Properties.LastModified.ToString());
+        }
+
+        static void SetMetadata(CloudBlobContainer container)
+        {
+            // Clear all metadata
+            container.Metadata.Clear();
+            // Add metadata on different ways
+            container.Metadata.Add("author", "the1bit");
+            container.Metadata["authoredOn"] = DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString();
+
+
+
+            // Apply changes
+            container.SetMetadata();
+        }
+
+        static void ListMetadata(CloudBlobContainer container)
+        {
+            // Get existing metadata
+            container.FetchAttributes();
+
+            Console.WriteLine("-=== METADATA ===-\n");
+
+            // Fetch on metadata list
+            foreach (var item in container.Metadata)
+            {
+                Console.WriteLine("Key: " + item.Key);
+                Console.WriteLine("Value: " + item.Value + "\n\n");
+            }
+        }
+
+        static void CopyBlob(CloudBlobContainer container, string fileName)
+        {
+            // Get source
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+
+            // Set target
+            CloudBlockBlob copyTobBockBlob = container.GetBlockBlobReference("copy_" + fileName);
+
+            // Start copy
+            copyTobBockBlob.StartCopyAsync(new Uri(blockBlob.Uri.AbsoluteUri));
+        }
+
+        static void CreateSAS(CloudBlobContainer container)
+        {
+            // Create a new access policy
+            SharedAccessBlobPolicy sharedPolicy = new SharedAccessBlobPolicy()
+            {
+                SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24),
+                Permissions = SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.Write | SharedAccessBlobPermissions.List
+            };
+
+            // Get container's existing permissions
+            BlobContainerPermissions permissions = new BlobContainerPermissions();
+
+            // Add the new policy to the container's permissions
+            permissions.SharedAccessPolicies.Clear();
+            permissions.SharedAccessPolicies.Add("PolicyName", sharedPolicy);
+            container.SetPermissions(permissions);
         }
     }
+
 }
